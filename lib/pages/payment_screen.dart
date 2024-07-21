c366094e27b19c71ef92109262e269c6a864e533
RutiKirsh <ruti4293@gmail.com>
2024-07-21 21:42:28 +0300

    started email in payment--to be continued�

--- lib/pages/payment_screen.dart
+++ lib/pages/payment_screen.dart
@@ -1,3 +1,4 @@
+import 'package:coffee_app/services/email_service.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_credit_card/flutter_credit_card.dart';
 
@@ -56,7 +57,7 @@ class _PaymentScreenState extends State<PaymentScreen> {
                                       ),
                                     );
                                     Navigator.pop(context);
-                                    // Navigator.pop(context, true);
+                                    // sendOrderEmail(email, order, total)
 
                                 }
                                 else{
